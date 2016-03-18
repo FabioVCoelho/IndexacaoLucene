71740d86be023cb6e6bf0c60a46ee9abcd7044ab
FabioVCoelho <angrazito@gmail.com>
2016-03-18 02:32:26 -0300

    Utilizando MultiFieldQueryParser para pesquisa
    Foi quebrado o MultiFieldQueryParser para não passar de 1024, o número máximo de possibilidades com o BooleanQuery utilizado pelo MultiFieldQueryParser

--- src/main/java/br/ufsc/IndexacaoLucene/MainLattesHandler.java
+++ src/main/java/br/ufsc/IndexacaoLucene/MainLattesHandler.java
@@ -3,6 +3,9 @@ package br.ufsc.IndexacaoLucene;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.xml.parsers.ParserConfigurationException;
 
@@ -13,8 +16,9 @@ import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.IndexableField;
+import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
 import org.apache.lucene.queryparser.classic.ParseException;
-import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
@@ -25,33 +29,65 @@ import org.xml.sax.SAXException;
 
 public class MainLattesHandler {
 
-public static void main(String[] args)
+	public static void main(String[] args)
 			throws IOException, ParserConfigurationException, SAXException, ParseException {
-		CampoCompleto handler = new CampoCompleto();
+		LattesHandler handler = new LattesHandler();
 		InputStream arquivoCurriculo = new FileInputStream("curriculos/Carina Lattes.xml");
+		InputStream arquivoCurriculo2 = new FileInputStream("curriculos/Adriano Lattes.xml");
+		InputStream arquivoCurriculo3 = new FileInputStream("curriculos/Christiane Lattes.xml");
 		Document doc = handler.getDocument(arquivoCurriculo);
+		Document doc2 = handler.getDocument(arquivoCurriculo2);
+		Document doc3 = handler.getDocument(arquivoCurriculo3);
 
 		Directory indexDir = new RAMDirectory();
 		Analyzer analyzer = new StandardAnalyzer();
 		IndexWriterConfig iwriterConf = new IndexWriterConfig(analyzer);
 		IndexWriter iwriter = new IndexWriter(indexDir, iwriterConf);
 		iwriter.addDocument(doc);
+		iwriter.addDocument(doc2);
+		iwriter.addDocument(doc3);
 		iwriter.close();
 
 		IndexReader reader = DirectoryReader.open(indexDir);
 		IndexSearcher searcher = new IndexSearcher(reader);
 
-		List<IndexableField> a = doc.getFields();
-		List<String> listaDeFields = new ArrayList<String>();
-		for (int i = 0; i < 1024; i++)
-			listaDeFields.add(a.get(i).name()); //Como o BooleanQuery chega no máximo a 1024 operações.
-		Object[] fieldsDoDocumento = listaDeFields.toArray(); // Utilizado para passar a List em String[] de Fields, que o MultiField recebe como parametro
-		String[] stringDasFields = Arrays.copyOf(fieldsDoDocumento, fieldsDoDocumento.length, String[].class); 
-		MultiFieldQueryParser queryParser = new MultiFieldQueryParser(stringDasFields, analyzer);
-		Query query = queryParser.parse("Carina Fridrich Dorneles");
-		TopDocs docs = searcher.search(query, 10);
+		List<IndexableField> fieldsDosDocs = new ArrayList<IndexableField>();
+		int numeroMaximoDeFields = 0;
+		for (int numeroDeDocumentos = 0; numeroDeDocumentos < reader.maxDoc(); numeroDeDocumentos++)
+			if (reader.document(numeroDeDocumentos).getFields().size() > numeroMaximoDeFields) {
+				fieldsDosDocs = reader.document(numeroDeDocumentos).getFields();
+				numeroMaximoDeFields = fieldsDosDocs.size();
+			}
+		List<String> fields = new ArrayList<String>();
+		List<ScoreDoc[]> hits = new ArrayList<ScoreDoc[]>();
 
-		ScoreDoc[] hits = docs.scoreDocs;
-		System.out.println(docs.totalHits);
+		int indexDeInicio = 0;
+		int indexFinal = 1024;
+
+		while (indexFinal < fieldsDosDocs.size()) {
+			for (int index = indexDeInicio; index < indexFinal; index++)
+				fields.add(fieldsDosDocs.get(index).name());
+			Object[] fieldsDoDocumento = fields.toArray();
+			String[] stringDasFields = Arrays.copyOf(fieldsDoDocumento, fieldsDoDocumento.length, String[].class);
+			MultiFieldQueryParser queryParser = new MultiFieldQueryParser(stringDasFields, analyzer);
+			Query query = queryParser.parse("Friedrich");
+			TopDocs docs = searcher.search(query, 10);
+			hits.add(docs.scoreDocs);
+			indexDeInicio = indexFinal;
+			indexFinal = indexFinal + 1024;
+			if (indexFinal >= fieldsDosDocs.size())
+				indexFinal = fieldsDosDocs.size();
+			fields = new ArrayList<String>();
+		}
+		System.out.println("Documento em que a Query foi encontra: ");
+		for (int i = 0; i < hits.size(); ++i) {
+			for (int j = 0; j < hits.get(i).length; j++) {
+				int docId = hits.get(i)[j].doc;
+				Document d = searcher.doc(docId);
+				System.out.println(
+						docId + " " + d.get("CURRICULO-VITAE/DADOS-GERAIS/NOME-COMPLETO") + " " + hits.get(i)[j].score);
+			}
+		}
+		reader.close();
 	}
 }
