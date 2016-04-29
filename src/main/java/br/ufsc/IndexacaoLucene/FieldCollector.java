c9a79191a77fdc89d813581a19d30c9b7cb55a53
FabioVCoelho <angrazito@gmail.com>
2016-04-28 21:52:03 -0300

    Extensao do Collector, com armazenamento dos fields

---
+++ src/main/java/br/ufsc/IndexacaoLucene/FieldCollector.java
@@ -0,0 +1,30 @@
+package br.ufsc.IndexacaoLucene;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.lucene.index.LeafReaderContext;
+import org.apache.lucene.search.Collector;
+import org.apache.lucene.search.LeafCollector;
+
+public class FieldCollector implements Collector {
+
+	private FieldCollectorData data;
+
+	public FieldCollector(String procurarPor) {
+		data  = new FieldCollectorData(procurarPor);
+	}
+
+	public LeafCollector getLeafCollector(LeafReaderContext context) throws IOException {
+		return data;
+	}
+
+	public boolean needsScores() {
+		return false;
+	}
+
+	// Retorna todos os fields encontrados com a string passada como Parametro no construtor.
+	public List<String> retornaFields() {
+		return data.retornaFields();
+	}
+}
---
+++ src/main/java/br/ufsc/IndexacaoLucene/FieldCollectorData.java
@@ -0,0 +1,35 @@
+package br.ufsc.IndexacaoLucene;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.search.LeafCollector;
+import org.apache.lucene.search.Scorer;
+
+public class FieldCollectorData implements LeafCollector {
+	private String procurarPor;
+	private List<String> fieldsEncontrado = new ArrayList<String>();
+
+	public FieldCollectorData(String procurarPor) {
+		this.procurarPor = procurarPor;
+	}
+
+	public void setScorer(Scorer scorer) throws IOException {
+	}
+
+	// Utiliza um SearcherSemFiltroDeDocumentos para somente um Documento que foi coletado
+	// pelo query. Uso try/catch pq não posso dar throw.
+	public void collect(int doc) throws IOException {
+			try {
+				fieldsEncontrado.addAll(new BuscaDeFields().retornarFields(doc, procurarPor));
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+	}
+
+	public List<String> retornaFields() {
+		return fieldsEncontrado;
+	}
+}
