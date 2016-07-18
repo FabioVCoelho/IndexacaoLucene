252711426367ca43fafded5c16ad946c52f141a1
FabioVCoelho <angrazito@gmail.com>
2016-07-18 17:40:11 -0300

    Hierarchical layout

--- src/main/java/br/ufsc/treeJgraphT/JGraphAdapterDemo.java
+++ src/main/java/br/ufsc/treeJgraphT/JGraphAdapterDemo.java
@@ -2,10 +2,10 @@ package br.ufsc.treeJgraphT;
 
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.geom.Rectangle2D;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import javax.swing.JApplet;
 import javax.swing.JFrame;
@@ -13,8 +13,6 @@ import javax.swing.JFrame;
 import org.apache.lucene.queryparser.classic.ParseException;
 import org.jgraph.JGraph;
 import org.jgraph.graph.AttributeMap;
-import org.jgraph.graph.DefaultGraphCell;
-import org.jgraph.graph.GraphConstants;
 import org.jgrapht.DirectedGraph;
 import org.jgrapht.Graph;
 import org.jgrapht.ListenableGraph;
@@ -22,15 +20,17 @@ import org.jgrapht.ext.JGraphModelAdapter;
 import org.jgrapht.graph.DefaultListenableGraph;
 import org.jgrapht.graph.DirectedMultigraph;
 
+import com.jgraph.layout.JGraphFacade;
+import com.jgraph.layout.hierarchical.JGraphHierarchicalLayout;
+
 import br.ufsc.IndexacaoLucene.SearcherComFiltroDeDocumentos;
 import br.ufsc.visualizao.SepararString;
 
 public class JGraphAdapterDemo extends JApplet {
 	private static final long serialVersionUID = 3256444702936019250L;
 	private static final Color DEFAULT_BG_COLOR = Color.decode("#FAFBFF");
-	private static final Dimension DEFAULT_SIZE = new Dimension(530, 320);
+	private static final Dimension DEFAULT_SIZE = new Dimension(600, 500);
 
-	//
 	private JGraphModelAdapter<String, Path> jgAdapter;
 	private List<String> camposRetornaveis;
 	private static JFrame frame;
@@ -56,14 +56,16 @@ public class JGraphAdapterDemo extends JApplet {
 
 	/**
 	 * {@inheritDoc}
+	 * 
+	 * @return
 	 */
+	@SuppressWarnings("unchecked")
 	public void init() {
 		// create a JGraphT graph
 		ListenableGraph<String, Path> g = new ListenableDirectedMultigraph<>(Path.class);
 		// create a visualization using JGraph, via an adapter
 		jgAdapter = new JGraphModelAdapter<>(g);
 		JGraph jgraph = new JGraph(jgAdapter);
-		adjustDisplaySettings(jgraph);
 		getContentPane().add(jgraph);
 		resize(DEFAULT_SIZE);
 
@@ -81,15 +83,9 @@ public class JGraphAdapterDemo extends JApplet {
 			g.addVertex(string);
 		}
 
-		// Adicionei um MouseListener no JGraph para mostrar todos os caminhos
+		// Adiciona um MouseListener no JGraph para mostrar todos os caminhos
 		// quando clicar no Retangulo.
 		jgraph.addMouseListener(new OuvinteDoMouse(jgraph, jgAdapter));
-
-		// Ordenação dos retangulos por pixel.
-		int x2 = 250;
-		int y2 = 10;
-		int y1 = 10;
-		int x1 = 10;
 
 		/*
 		 * Para todos os caminhos encontrados com o SepararString, é criado um
@@ -103,71 +99,29 @@ public class JGraphAdapterDemo extends JApplet {
 			jgAdapter.getVertexCell(vertex).getAttributes().put("path", "");
 		}
 		for (int i = 0; i < pathsSeparados.size() - 1; i++) {
-			if (pathsSeparados.get(i + 1).equals("CURRICULO-VITAE"))
-				i++;
-			if (!g.containsEdge(pathsSeparados.get(i), pathsSeparados.get(i + 1))) {
-				g.addEdge(pathsSeparados.get(i), pathsSeparados.get(i + 1));
-				positionVertexAt(pathsSeparados.get(i), x1, y1);
-				positionVertexAt(pathsSeparados.get(i + 1), x2, y2);
+			if (!g.containsEdge(pathsSeparados.get(i), pathsSeparados.get(i + 1))
+					&& !pathsSeparados.get(i + 1).equals("CURRICULO-VITAE")) {
 				/*
 				 * O vertex possui um HashMap e para guardar todos os caminhos
 				 * que o vertice seguiu criei um "path" no Map e guardei os
 				 * caminhos.
 				 */
-				jgAdapter.getVertexCell(pathsSeparados.get(i)).getAttributes().put("path",
-						jgAdapter.getVertexCell(pathsSeparados.get(i)).getAttributes().get("path")
-								+ pathsSeparados.get(i) + "->" + pathsSeparados.get(i + 1) + "  ");
-
-				y1 = y1 + 70;
-				y2 = y2 + 70;
-				if (y1 > 600) {
-					x1 = x1 + 70;
-					y1 = 10;
-				}
-				if (y2 > 600) {
-					x2 = x2 + 70;
-					y2 = 10;
-				}
+				AttributeMap atributosDoVertice = jgAdapter.getVertexCell(pathsSeparados.get(i)).getAttributes();
+				atributosDoVertice.put("path", atributosDoVertice.get("path") + pathsSeparados.get(i) + "->"
+						+ pathsSeparados.get(i + 1) + "  ");
+				g.addEdge(pathsSeparados.get(i), pathsSeparados.get(i + 1));
 			}
 		}
-	}
+		final JGraphFacade jgf = new JGraphFacade(jgraph);
+		final JGraphHierarchicalLayout layoutifier = new JGraphHierarchicalLayout();
+		layoutifier.run(jgf);
+		System.out.println("Layout complete");
 
-	private void adjustDisplaySettings(JGraph jg) {
-		jg.setPreferredSize(DEFAULT_SIZE);
+		final Map<?, ?> nestedMap = jgf.createNestedMap(true, true);
+		jgraph.getGraphLayoutCache().edit(nestedMap);
 
-		Color c = DEFAULT_BG_COLOR;
-		String colorStr = null;
-
-		try {
-			colorStr = getParameter("bgcolor");
-		} catch (Exception e) {
-		}
-
-		if (colorStr != null) {
-			c = Color.decode(colorStr);
-		}
-
-		jg.setBackground(c);
-	}
-
-	/*
-	 * Método retirado da demo. Posiciona o vertice nas coordenadas passadas
-	 * como x e y.
-	 */
-	@SuppressWarnings("unchecked")
-	private void positionVertexAt(Object vertex, int x, int y) {
-		DefaultGraphCell cell = jgAdapter.getVertexCell(vertex);
-		AttributeMap attr = cell.getAttributes();
-		Rectangle2D bounds = GraphConstants.getBounds(attr);
-
-		Rectangle2D newBounds = new Rectangle2D.Double(x, y, bounds.getWidth(), bounds.getHeight());
-
-		GraphConstants.setBounds(attr, newBounds);
-
-		// TODO: Clean up generics once JGraph goes generic
-		AttributeMap cellAttr = new AttributeMap();
-		cellAttr.put(cell, attr);
-		jgAdapter.edit(cellAttr, null, null, null);
+		jgraph.getGraphLayoutCache().update();
+		jgraph.refresh();
 	}
 
 	/**
